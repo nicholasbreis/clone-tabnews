648f9057b2c2732559f461ece57d79b599931eb0
nicholasbreis <164106536+nicholasbreis@users.noreply.github.com>
2026-03-06 02:43:55 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste Novo  😎 </h1>
-} 
+  return <h1>Teste Novo 😎 </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 
 export default Home;
